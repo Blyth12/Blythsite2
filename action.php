54cfdc87e087019d54fbbd251f79948afd78e401
Elliott Blyth Larrubia <elliottblyth@gmail.com>
2023-09-01 17:27:34 +0100

    login functional

--- action.php
+++ action.php
@@ -1,50 +1,58 @@
 <?php
-
-// Connect to the database
+//Conn
 $host = "localhost:3306";
 $username = "root";
 $password = "";
 $dbname = "accounts";
-
 $conn = mysqli_connect($host, $username, $password, $dbname);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// Check if the form has been submitted
+//Submit check
 if (isset($_POST['submit'])) {
-    header("Location: login.html");
     // Get the form data
     $username = mysqli_real_escape_string($conn, $_POST['username']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
 
+    //Query
     $query = "SELECT * FROM accounts WHERE username = '$username' AND password = '$password'";
-    // Execute query
     $result = mysqli_query($conn, $query);
 
-    // Check if the query returned any results
+    //Check if the query returned any results
     if (mysqli_num_rows($result) > 0) {
-        // Match
+        //Match
         mysqli_close($conn);
         echo "Login successful!";
+        $loggedin = array(
+            'success' => true,
+            'message' => 'Login successful'
+        );
+        $loggedinJSON = json_encode($loggedin);
         header("Location: uploads.html");
         exit();
     }
     else {
-        // No match
+        //No match
         mysqli_close($conn);
         echo "Login failed. Incorrect username, password, or acckey.";
-        header("Location: login.html");
+        header("Location: login.html?error=credentials");
+        $loggedin = array(
+            'success' => false,
+            'message' => 'Login failed'
+        );
         exit();
     }
 }
 
 else {
-    header("Location: index.html");
+    header("Location: login.html?error=connection");
     exit();
 }
 
+header('Content-Type: application/json');
+echo json_encode($loggedin);
 mysqli_close($conn);
 
 ?>
--- logcheck.js
+++ logcheck.js
@@ -1,6 +1,19 @@
-const loggedin = false;
-// replace above w/ login stimulus
+$.ajax({
+    url: 'action.php',
+    method: 'POST',
+    dataType: 'json',
+    success: function(data) {
+        console.log(data)
+        if (data.success === true) {
+            console.log ('Logged in: ' + data.message); 
+        }
+        else {
+            console.log('Login failed.');
+            window.location.href = 'login.html';
+        }
+    },
+    error: function() {
+        console.log('Error');
+    }
 
-if (!loggedin) {
-    window.location.href = "login.html";
-}
+});
